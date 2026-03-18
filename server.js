9d8a90eb358772507672137ffdac1aafd6ab1e23
Immanuel <immanuel4partner@gmai.com>
2026-03-19 00:11:38 +0100

    site deployed

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
@@ -15,28 +16,25 @@ const path = require("path")
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
 // Index Route
-app.get("/",function(req, res){
-  res.render("index", {title: "Home"})
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
 })
-
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000  
+const host = process.env.HOST || "localhost"
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Server running at http://${host}:${port}`)
 })
