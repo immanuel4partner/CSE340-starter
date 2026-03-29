10ebf51332f3ddc8291e73747b1bf840e47c42a1
Immanuel <immanuel4partner@gmai.com>
2026-03-29 02:41:57 +0100

    Update pnpm lockfile and server.js for Render

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(async (err, req, res, next) => {
 /* ***********************
  * Local Server Information
  *************************/
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5500
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`app listening on port ${port}`)
